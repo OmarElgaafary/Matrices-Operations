414b7e46bfdfd9d9249fb705ec4b94816bc9d8ca
OmarElgaafary <omarelgaafaryy@gmail.com>
2024-07-25 08:32:09 +0300

    Update matrices.cpp

--- matrices.cpp
+++ matrices.cpp
@@ -118,54 +118,44 @@ void matrixSub() {
 }
 
 void matrixMul() {
-	int m1[2][2]; // declaration
-	int m2[2][2];
-	int multiplication[2][2];
+int m1[2][2];
+int m2[2][2];
+int multiplication[2][2] = { {0, 0}, {0, 0} };
 
-	std::cout << "Enter values for M1 (2x2): "; // m1 input
+std::cout << "Enter values for m1 (2x2): ";
+for (int i = 0; i < 2; i++) {
+	for (int j = 0; j < 2; j++) {
+		std::cin >> m1[i][j];
+	}
+}
+std::cout << "Enter values for m2 (2x2): ";
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			std::cin >> m1[i][j];
+for (int i = 0; i < 2; i++) {
+	for (int j = 0; j < 2; j++) {
+		std::cin >> m2[i][j];
+	}
+}
+
+for (int i = 0; i < 2; i++) { // i = rows
+	for (int j = 0; j < 2; j++) { // j = col
+		for (int x = 0; x < 2; x++) { // x = index of current row / col
+			multiplication[i][x] = multiplication[i][x] + m1[i][x] * m2[x][j]; // m1 rows multiplied by m2 cols
 		}
 	}
+}
 
+for (int i = 0; i < 2; i++) {
+	std::cout << "[";
+	for (int j = 0; j < 2;j++) {
+		std::cout << multiplication[i][j];
+		if (j <= 0) {
+			std::cout << ", ";
+		}
+
+	}
+	std::cout << "]";
 	std::cout << std::endl;
-	std::cout << "Enter values for M2 (2x2): "; // m2 input
-
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			std::cin >> m2[i][j];
-		}
-	}
-
-
-
-	for (int i = 0; i < 2; i++) { // multiplication operation
-
-		for (int j = 0; j < 2; j++) {
-
-			multiplication[i][j] = m1[i][j] * m2[i][j];
-
-		}
-	}
-
-
-	for (int x = 0; x < 2; x++) { // prints the product of the two matrices
-
-		std::cout << "[";
-
-		for (int z = 0; z < 2; z++) {
-
-
-			std::cout << multiplication[x][z];
-			if (z <= 0) {
-				std::cout << ", ";
-			}
-		}
-		std::cout << "]";
-		std::cout << std::endl;
-	}
+}
 }
 
 void matrixPrint() {
